7dcaa1909282c36b959fdf886d36f9ebde1cde0b
Daniel <danielvallecilloinformatica@gmail.com>
2018-01-22 17:43:41 +0100

    Code Labs completed

--- app/src/main/java/com/google/firebase/example/fireeats/MainActivity.java
+++ app/src/main/java/com/google/firebase/example/fireeats/MainActivity.java
@@ -35,7 +35,10 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.example.fireeats.adapter.RestaurantAdapter;
+import com.google.firebase.example.fireeats.model.Restaurant;
+import com.google.firebase.example.fireeats.util.RestaurantUtil;
 import com.google.firebase.example.fireeats.viewmodel.MainActivityViewModel;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -55,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements
 
     private static final int RC_SIGN_IN = 9001;
 
-    private static final int LIMIT = 50;
+    private static final int LIMIT = 50; //Indicates the nº of restaurants retrieved
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
@@ -73,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements
     ViewGroup mEmptyView;;
 
     private FirebaseFirestore mFirestore;
-    private Query mQuery;
+    private Query mQuery; //Used to save queries
 
     private FilterDialogFragment mFilterDialog;
     private RestaurantAdapter mAdapter;
@@ -102,7 +105,12 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void initFirestore() {
-        // TODO(developer): Implement
+        mFirestore = FirebaseFirestore.getInstance();
+
+        // Get the 50 highest rated restaurants
+        mQuery = mFirestore.collection("restaurants")
+                .orderBy("avgRating", Query.Direction.DESCENDING)
+                .limit(LIMIT);
     }
 
     private void initRecyclerView() {
@@ -163,15 +171,52 @@ public class MainActivity extends AppCompatActivity implements
         }
     }
 
+    //Add random restaurants to the DB
     private void onAddItemsClicked() {
-        // TODO(developer): Add random restaurants
-        showTodoToast();
+        // Get a reference to the restaurants collection
+        CollectionReference restaurants = mFirestore.collection("restaurants");
+
+        for (int i = 0; i < 10; i++) {
+            // Get a random Restaurant POJO
+            Restaurant restaurant = RestaurantUtil.getRandom(this);
+
+            // Add a new document to the restaurants collection
+            restaurants.add(restaurant);
+        }
     }
 
+    //Accepts a filter object
     @Override
     public void onFilter(Filters filters) {
-        // TODO(developer): Construct new query
-        showTodoToast();
+        // Construct query basic query
+        Query query = mFirestore.collection("restaurants");
+
+        // Category (equality filter)
+        if (filters.hasCategory()) {
+            query = query.whereEqualTo("category", filters.getCategory());
+        }
+
+        // City (equality filter)
+        if (filters.hasCity()) {
+            query = query.whereEqualTo("city", filters.getCity());
+        }
+
+        // Price (equality filter)
+        if (filters.hasPrice()) {
+            query = query.whereEqualTo("price", filters.getPrice());
+        }
+
+        // Sort by (orderBy with direction)
+        if (filters.hasSortBy()) {
+            query = query.orderBy(filters.getSortBy(), filters.getSortDirection());
+        }
+
+        // Limit items
+        query = query.limit(LIMIT);
+
+        // Update the query
+        mQuery = query;
+        mAdapter.setQuery(query);
 
         // Set header
         mCurrentSearchView.setText(Html.fromHtml(filters.getSearchDescription(this)));
@@ -181,12 +226,14 @@ public class MainActivity extends AppCompatActivity implements
         mViewModel.setFilters(filters);
     }
 
+    //Inflates the menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    //What happens with the click in the menu items
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
